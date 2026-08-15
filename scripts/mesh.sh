a0644586515f978a5c65dc3a4f47bcfa55155101
Pravin Carthic <56361904+pravincarthic@users.noreply.github.com>
2026-08-15 21:28:33 +0530

    Update OMP_NUM_THREADS and output log file

--- scripts/mesh.sh
+++ scripts/mesh.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export OMP_NUM_THREADS=64
-nohup /home/pravin/SALOME-9.16.0/salome -b ./Gmsh_cloud_run.py > ./meshing_output.log 2>&1 &
+export OMP_NUM_THREADS=28
+nohup env PYTHONUNBUFFERED=1 /home/pravin/SALOME-9.16.0/salome -b ./Gmsh_cloud_run.py > ./output_meshing.log 2>&1 &
