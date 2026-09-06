dbc34d72ab9ec70cfdde651c842fb6eeb1053713
Pravin Carthic <56361904+pravincarthic@users.noreply.github.com>
2026-09-06 17:35:26 +0530

    Script updated, Config tweaked, Step File Snapshot taken

--- scripts/meshing.bat
+++ scripts/meshing.bat
@@ -1,5 +1,5 @@
-cd "D:\BWB Spaceplane\Meshing Scripts\V2_cleaned"
+cd "D:\BWB_git\BWB_Spaceplane\scripts"
 d:
 set PYTHONUNBUFFERED=1
 
-python3 final_meshing_sizefield_updated.py gmsh_config_sizefield.json > log.log 2>&1
+python3 final_meshing.py gmsh_config.json > log.log 2>&1
